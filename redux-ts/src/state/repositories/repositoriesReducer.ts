27f660005c2bbf1d7a4937ef109465678b51497c
Pyunghyuk Yoo <yoophi@gmail.com>
2021-04-08 15:39:28 +0900

    ✨ IMPROVE: action as interface

--- redux-ts/src/state/repositories/repositoriesReducer.ts
+++ redux-ts/src/state/repositories/repositoriesReducer.ts
@@ -4,14 +4,22 @@ interface RepositoriesState {
   data: string[];
 }
 
-const reducer = (state: RepositoriesState, action: any): RepositoriesState => {
+interface Action {
+  type: string;
+  payload?: any;
+}
+
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
   switch (action.type) {
     case "search_repositories":
       return { loading: true, error: null, data: [] };
-      case 'search_repositories_success':
-      return {loading: false, error: null, data: action.payload};
-      case 'search_repositories_error':
-      return {loading: false, error: action.payload, data: []};
+    case "search_repositories_success":
+      return { loading: false, error: null, data: action.payload };
+    case "search_repositories_error":
+      return { loading: false, error: action.payload, data: [] };
     default:
       return state;
   }
