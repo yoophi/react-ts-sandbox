98d6a15facfd771a5e47cfa65e8b36e952d79b2e
Pyunghyuk Yoo <yoophi@gmail.com>
2021-04-11 21:45:45 +0900

    ✨ UPDATE: init esbuild

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,9 +1,24 @@
+import * as esbuild from "esbuild-wasm";
+
+import { useEffect, useState } from "react";
+
 import ReactDOM from "react-dom";
-import { useState } from "react";
 
 const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
+
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+    console.log(service);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
 
   const onClick = () => {
     console.log(input);
