c1cc9d76eb7af4f58adf2aa05658f3897b2e947f
Pyunghyuk Yoo <yoophi@gmail.com>
2021-04-16 22:08:46 +0900

    🎨  UPDATE: reformat code

--- redux-pokemon-client/src/components/Search.tsx
+++ redux-pokemon-client/src/components/Search.tsx
@@ -2,9 +2,9 @@ import React, { ChangeEvent, useState } from "react";
 
 const Search = () => {
   const [pokemonName, setPokemonName] = useState("");
-
   const handleChange = (e: ChangeEvent<HTMLInputElement>) =>
     setPokemonName(e.target.value);
+
   return (
     <div>
       <input type="text" onChange={handleChange} />
